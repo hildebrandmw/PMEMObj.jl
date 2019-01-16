283d2c5a1965c95f1b3d6eb87f8390c7680a1459
Mark Hildebrand <mhildebrand@ucdavis.edu>
2019-01-16 17:04:07 +0000

    Making headway

--- src/persist.jl
+++ src/persist.jl
@@ -18,10 +18,10 @@ using ..Transaction
 
 abstract type AbstractClass end
 
-struct NoClass <: AbstractClass end
+struct Fallback <: AbstractClass end
 struct IsBits <: AbstractClass end
 
-class(::Type{T}) where {T} = isbitstype(T) ? IsBits() : NoClass()
+class(::Type{T}) where {T} = isbitstype(T) ? IsBits() : Fallback()
 class(x::T) where {T} = class(T)
 
 """
@@ -39,29 +39,50 @@ end
 getoid(P::Persistent) = getfield(P, :__oid)
 getpool(P::Persistent) = Lib.pool(getoid(P))
 
+
 include("array.jl")
-
-# TODO: Automagic this with a macro?
-aspersistent(::Type{T}) where {T} = Persistent{T}
-asvolatile(::Type{T}) where {T} = T
-
-aspersistent(::Type{Array{T,N}}) where {T,N} = Persistent{ArrayHandle{T,N}}
-asvolatile(::Type{ArrayHandle{T,N}}) where {T,N} = Array{T,N}
 
 #####
 ##### persist
 #####
 
-persist(pool, x::T) where {T} = persist(pool, x, class(T))
-
-persist(pool, x::T, trait::AbstractClass) where {T} = transaction(() -> store(pool, x, trait), pool)
+persist(pool, x::T) where {T} = transaction(() -> store(pool, x), pool)
 
 #####
 ##### Store
 #####
 
-store(pool, x::T, ::AbstractClass) where {T <: Union{String,Symbol}} = Persistent(Lib.tx_strdup(String(x), T))
+# Special behavior for strings and arrays
+store(pool, x::String) = Persistent(Lib.tx_strdup(x, String))
+store(pool, x::Symbol) = Persistent(Lib.tx_strdup(String(x), Symbol))
 
+# arrays
+store(pool, x::Array{T,N}) where {T,N} = _store(pool, x, Val{isbitstype(T)}())
+
+# bits types
+function _store(pool, x::Array{T,N}, ::Val{true}) where {T,N}
+    oid = Lib.tx_alloc(sizeof(x), T)
+    ptr = Lib.direct(oid)
+
+    unsafe_copyto!(ptr, pointer(x), length(x))
+
+    return persist(pool, ArrayHandle{T,N}(size(x), oid))
+end
+
+# non bits types
+function _store(pool, x::Array{T,N}, ::Val{false}) where {T,N}
+    oid = Lib.tx_alloc(length(x) * sizeof(Persistent{Nothing}), Persistent{T})
+    ptr = Lib.direct(oid)
+
+    for (index, item) in enumerate(x)
+        unsafe_store!(ptr, store(pool, item), index)
+    end
+
+    return persist(pool, ArrayHandle{Persistent{T},N}(size(x), oid))
+end
+
+# Fallback - check if isbits
+store(pool, x::T) where {T} = store(pool, x::T, class(T))
 function store(pool, x::T, ::IsBits) where {T}
     oid = Lib.tx_alloc(sizeof(x), T)
     ptr = Lib.direct(oid)
@@ -70,110 +91,64 @@ function store(pool, x::T, ::IsBits) where {T}
     return Persistent(oid)
 end
 
-function store(pool, x::Array{T,N}, ::AbstractClass) where {T,N}
-    oid = Lib.tx_alloc(sizeof(x), T)
+function store(pool, x::T, ::Fallback) where {T}
+    # Create OIDs for every field of `x`
+    oid = Lib.tx_alloc(nfields(x) * sizeof(Persistent{T}), T)
     ptr = Lib.direct(oid)
-    unsafe_copyto!(ptr, pointer(x), length(x))
 
-    handle = persist(pool, ArrayHandle{T,N}(size(x), oid))
-    return Persistent(handle)
+    # Store all members of `x`
+    for (index, field) in enumerate(fieldnames(T))
+        object = store(pool, getfield(x, field))
+        thisptr = convert(Ptr{typeof(object)}, ptr) + (index - 1) * sizeof(Persistent{T})
+        unsafe_store!(thisptr, object)
+    end
+    return Persistent{T}(oid)
 end
 
 #####
 ##### retrieve
 #####
 
-retrieve(x::Persistent{T}) where {T} = retrieve(x, class(T))
-retrieve(x::Persistent, trait::AbstractClass) = retrieve(Lib.direct(getoid(x)), trait)
+retrieve(x::Persistent) = retrieve(Lib.direct(getoid(x)))
 
 # Specializtions
-retrieve(ptr::Ptr{T}, ::AbstractClass) where {T} = asvolatile(T)(unsafe_string(convert(Ptr{UInt8}, ptr)))
-retrieve(ptr::Ptr{T}, ::IsBits) where {T} = unsafe_load(convert(Ptr{asvolatile(T)}, ptr))
+retrieve(ptr::Ptr{String}) = unsafe_string(convert(Ptr{UInt8}, ptr))
+retrieve(ptr::Ptr{Symbol}) = Symbol(unsafe_string(convert(Ptr{UInt8}, ptr)))
 
-# Need to mark as "isbits" since class(ArrayHandle) returns IsBits
-function retrieve(ptr::Ptr{ArrayHandle{T,N}}, ::IsBits) where {T,N}
+function retrieve(ptr::Ptr{ArrayHandle{T,N}}) where {T,N} 
     handle = unsafe_load(ptr)
-    A = asvolatile(ArrayHandle{T,N})(undef, handle.size)
+    A = Array{T,N}(undef, handle.size)
     unsafe_copyto!(pointer(A), Lib.direct(handle.base), prod(handle.size))
     return A
 end
 
-# # Generic fallback
-# function retrieve(x::Persistent{T}, ::IsOther) where {T}
-#     offsets = fieldoffsets(x)
-#     ptr = Lib.direct(getoid(x))
-#     fields = []
-# 
-#     for index = 1:fieldcount(T)
-#         _ptr = ptr + offsets[index]
-#         _type = fieldtype(T, index)
-#         trait = class(_type)
-# 
-#         field = _retrieve(convert(Ptr{_type}, _ptr), trait)
-#         push!(fields, field)
-#     end
-#     return T(fields...)
-# end
+function retrieve(ptr::Ptr{ArrayHandle{Persistent{T},N}}) where {T,N}
+    handle = unsafe_load(ptr)
+    A = Array{T,N}(undef, handle.size)
 
-# _retrieve(ptr::Ptr, trait::IsBits) = retrieve(ptr, trait)
-# function _retrieve(ptr::Ptr{T}, trait::AbstractClass) where {T} 
-#     retrieve(convert(Ptr{aspersistent(T)}, ptr), trait)
-# end
-
-#####
-##### Field Calculations
-#####
-
-_fieldnames(P::Persistent{T}) where {T} = fieldnames(T)
-
-# For now, just tack on enough room for the last element to be a full pointer. Eventually
-# we'll revisit this assumption
-_allocsize(::Type{T}) where {T} = sizeof(PersistentOID{T}) + last(fieldoffsets(Persistent{T}))
+    base = Lib.direct(handle.base)
+    for index in 1:length(A)
+        A[index] = retrieve(unsafe_load(base, index))
+    end
+    return A
+end
 
 
-# function _fieldoffsets(::Type{Persistent{T}}) where {T}
-#     # Accumulate shifts we had to do to accomodate the extra room for persistent pointers
-#     shift = 0
-#     count = Tuple(1:fieldcount(T))
-#     offsets = map(count) do index
-#         _name = fieldname(T, index)
-#         _type = fieldtype(T, index)
-#         _offset = fieldoffset(T, index)
-# 
-#         if isbitstype(_type) || index == fieldcount(T)
-#             return _offset + shift
-#         else
-#             # Get the size allocated for this field.
-#             fieldsize = fieldoffset(T, index + 1) - _offset 
-# 
-#             # Compute the offset of this field, then compute how much we need to shift
-#             # everything else below this.
-#             thisoffset = _offset + shift
-# 
-#             shift += max(signed(sizeof(PersistentOID{T}) - fieldsize), 0)
-#             return thisoffset
-#         end
-#     end
-#     return NamedTuple{fieldnames(T)}(offsets)
-# end
-# 
-# @generated function fieldoffsets(::Type{Persistent{T}}) where {T}
-#     offsets = _fieldoffsets(Persistent{T})
-#     return :($offsets)
-# end
-# fieldoffsets(P::Persistent{T}) where {T} = fieldoffsets(Persistent{T})
-# 
-# function getproperty(P::Persistent{T}, name::Symbol) where {T}
-#     baseptr = Lib.direct(getoid(P))
-#     offsets = fieldoffsets(P)
-#     _type = fieldtype(T, name)
-#     if isbitstype(_type)
-#         ptr = convert(Ptr{_type}, baseptr)
-#         return unsafe_load(ptr + offsets[name])
-#     else
-#         ptr = convert(Ptr{Persistent{_type}}, baseptr)
-#         return unsafe_load(ptr + offsets[name])
-#     end
-# end
+
+# Fallback
+retrieve(ptr::Ptr{T}) where {T} = retrieve(ptr::Ptr{T}, class(T))
+retrieve(ptr::Ptr{T}, ::IsBits) where {T} = unsafe_load(ptr)
+
+function retrieve(ptr::Ptr{T}, ::Fallback) where {T}
+    fields = []
+    for (index, field) in enumerate(fieldnames(T))
+        # Need to make an OID from the 16 bytes here to get the actual object.
+        ptr_offset = ptr + (index - 1) * sizeof(Persistent{T})
+        handle = unsafe_load(convert(Ptr{Persistent{fieldtype(T, field)}}, ptr_offset))
+
+        push!(fields, retrieve(handle))
+    end
+    return T(fields...)
+end
 
 end
